daacd3b37aa005994531fb6650cb5098e14e2ecb
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2022-08-26 19:08:09 +0530

    frontend and backend workflow setup

---
+++ backend/index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect("mongodb://localhost:27017/bloggz")
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    publicationDate: String,
+    content: String
+})
+
+const Post = new mongoose.model("Post", postSchema)
+
+const post1 = new Post({
+    title: "React express workflow setup guide",
+    author: "John Doe",
+    publicationDate: "12/03/2021",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nam id, quasi culpa saepe totam iure laudantium numquam soluta explicabo illum dolorem quas quia sint nulla, voluptate nihil. Nobis repellendus vel quo eius inventore exercitationem quis labore molestiae debitis eveniet!"
+}) 
+const post2 = new Post({
+    title: "How to build design system in figma",
+    author: "Jennifer Walter",
+    publicationDate: "20/06/2022",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nam id, quasi culpa saepe totam iure laudantium numquam soluta explicabo illum dolorem quas quia sint nulla, voluptate nihil. Nobis repellendus vel quo eius inventore exercitationem quis labore molestiae debitis eveniet!"
+}) 
+const post3 = new Post({
+    title: "practical guide to web development using react",
+    author: "Mathew Murdock",
+    publicationDate: "23/05/2022",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nam id, quasi culpa saepe totam iure laudantium numquam soluta explicabo illum dolorem quas quia sint nulla, voluptate nihil. Nobis repellendus vel quo eius inventore exercitationem quis labore molestiae debitis eveniet!"
+}) 
+
+const defaultPosts = [post1, post2, post3]
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get("/posts", (req, res) => {
+    // res.sendFile(__dirname + "/index.html")
+    Post.find((err, posts) => {
+        if(posts.length === 0){
+            Post.insertMany(defaultPosts, (err) => {
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("default posts inserted successfully")
+                }
+            })
+            res.redirect("/")
+        } else {
+            res.json(posts)
+        }
+    })
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log(`server up and running on port: ${PORT}`)
+})
